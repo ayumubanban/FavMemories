5ae5c72bba23b132befe21eb6a3d823330b9338b
ayumu <ayumuban@outlook.com>
2019-10-21 14:53:11 +0900

    user implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index' => "users#index"
+  get "users/:id" => "users#show"
+  get "signup" => "users#new"
+  post "users/create" => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
+
   get 'posts/index' => "posts#index"
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
