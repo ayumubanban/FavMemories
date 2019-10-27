c4061231b87486c85534e4d1cefb63a4c6e70302
ayumu <ayumuban@outlook.com>
2019-10-27 21:21:37 +0900

    bulmaのCDNを導入

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,29 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
-  # describe "GET /users" do
-  #   it "works! (now write some real specs)" do
-  #     get users_path
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
 
-  describe "POST #create" do
+  describe "GET users#new" do
+    it "リクエストが成功すること" do
+      get "/signup"
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "POST users#create" do
     let(:user) { FactoryBot.create(:user) }
-    # let(:valid_parameters) do
-    #   { name: user.name, email: user.email, password: user.password }
-    # end
 
     it "saves the user ID to the session object" do
-      # post "/users/create", session: valid_parameters
-      # post "/users/create", params: valid_parameters
       post "/users/create", params: { user: FactoryBot.attributes_for(:user) }
-      # post "/users/create", params: { user: user }
-      # post "/users/create", params: { user: valid_parameters }
 
-      # expect(session[:user_id]).to eq user.id
       expect(session[:user_id]).to eq User.last.id
     end
+  end
+
+  describe "GET users#login_form" do
+
+  end
+
+  describe "POST users#login" do
+
+  end
+
+  describe "POST users#logout" do
+
+  end
+
+  describe "GET users#index" do
+
+  end
+
+  describe "GET users#show" do
+
+  end
+
+  describe "GET users#edit" do
+
+  end
+
+  describe "POST users#update" do
+
+  end
+
+  describe "GET users#likes" do
+
+  end
+
+  describe "POST relationships#create" do
+
+  end
+
+  describe "POST relationships#destroy" do
+
+  end
+
+  describe "GET users#follows" do
+
+  end
+
+  describe "GET users#followers" do
+
   end
 
 end
