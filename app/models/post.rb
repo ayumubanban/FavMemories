3fb8173faafc0e6262f4636e97d0d4b5dc454e19
ayumu <ayumuban@outlook.com>
2019-10-29 15:14:58 +0900

    userとpostsのアソシエーション見直し

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,14 @@ class Post < ApplicationRecord
   validates :user_id, { presence: true }
 
   # * ユーザー
-  # belongs_to :user
+  belongs_to :user
 
   # * コメント
   has_many :comments
 
-  def user
-    # * インスタンスメソッド内で、selfはそのインスタンス自身を指す
-    # * post belongs to userっていう従属関係を自分らでメソッド定義して表してる
-    return User.find_by(id: self.user_id)
-  end
+  # def user
+  #   # * インスタンスメソッド内で、selfはそのインスタンス自身を指す
+  #   # * post belongs to userっていう従属関係を自分らでメソッド定義して表してる
+  #   return User.find_by(id: self.user_id)
+  # end
 end
